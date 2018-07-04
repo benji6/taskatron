255388208222f492ef4875b683c28bf51d66dbc7
Ben Hall <benji2357@gmail.com>
2018-07-04 14:57:19 +0100

    feat(SignOut): move button out of header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,21 +1,9 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { userSignOut } from '../../actions'
-import { userIsSignedInSelector } from '../../selectors'
-import IStore from '../../types/IStore'
-import { Button } from '../generic'
 import './style.css'
 
-interface IProps {
-  readonly isSignedIn: boolean
-  readonly onSignOut: typeof userSignOut
-}
-
-class Header extends React.PureComponent<IProps> {
+class Header extends React.PureComponent {
   public render(): React.ReactNode {
-    const { isSignedIn, onSignOut } = this.props
-
     return (
       <header className="header">
         <h1 className="header__heading">
@@ -23,23 +11,9 @@ class Header extends React.PureComponent<IProps> {
             Taskatron
           </Link>
         </h1>
-        <div className="header__profile">
-          {isSignedIn && <Button onClick={onSignOut}>Sign out</Button>}
-        </div>
       </header>
     )
   }
 }
 
-const mapStateToProps = (state: IStore) => ({
-  isSignedIn: userIsSignedInSelector(state),
-})
-
-const mapDispatchToProps = {
-  onSignOut: userSignOut,
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Header)
+export default Header
--- client/src/components/pages/Home/index.tsx
+++ client/src/components/pages/Home/index.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { userSignOut } from '../../../actions'
 import { userEmailSelector } from '../../../selectors'
 import IStore from '../../../types/IStore'
 import { Button, Link, Main, Paragraph } from '../../generic'
 
 interface IProps {
-  email?: string
+  readonly email: string | undefined
+  readonly onSignOut: typeof userSignOut
 }
 
 class Home extends React.PureComponent<IProps> {
   public render(): React.ReactNode {
-    const { email } = this.props
+    const { email, onSignOut } = this.props
 
     return (
       <>
         <Main>
           <Paragraph>This is home.</Paragraph>
           {email ? (
-            <Paragraph>Welcome back {email}!</Paragraph>
+            <>
+              <Paragraph>Welcome back {email}!</Paragraph>
+              <Button onClick={onSignOut}>Sign out</Button>
+            </>
           ) : (
             <>
               <Link to="sign-up">
@@ -37,8 +42,15 @@ class Home extends React.PureComponent<IProps> {
   }
 }
 
-const mapStateToProps = (state: IStore): IProps => ({
+const mapStateToProps = (state: IStore) => ({
   email: userEmailSelector(state),
 })
 
-export default connect(mapStateToProps)(Home)
+const mapDispatchToProps = {
+  onSignOut: userSignOut,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Home)
