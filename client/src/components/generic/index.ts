8d348a5098854705757c1aec2ba5a6beededd12f
Ben Hall <benji2357@gmail.com>
2018-07-22 23:55:12 +0100

    feat(Checkbox): adds Checkbox component

--- client/src/components/generic/index.ts
+++ client/src/components/generic/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button'
 export { default as ButtonGroup } from './ButtonGroup'
+export { default as Checkbox } from './Checkbox'
 export { default as Form } from './Form'
 export { default as Heading } from './Heading'
 export { default as Link } from './Link'
