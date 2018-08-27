4eb1da33bc6ecff36459f922950455b4b1adbb6c
Ben Hall <benji2357@gmail.com>
2018-08-27 22:35:45 +0100

    refactor(api:me): use async/await

--- api/src/controllers/me.ts
+++ api/src/controllers/me.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from 'express'
 import { getUser } from '../model/user'
 import pino from '../pino'
-import { IUserDocument } from '../shared/types'
 
 interface IRequest extends Request {
   user: string
 }
 
-export const get = (req: Request, res: Response): void => {
-  getUser((req as IRequest).user)
-    .then((user?: IUserDocument) => {
-      if (!user) throw Error('user not found')
-      res.status(200).send(user)
-    })
-    .catch((err: Error) => {
-      res.status(500).end()
-      pino.error(err)
-    })
+export const get = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await getUser((req as IRequest).user)
+
+    if (!user) return res.status(404).end()
+
+    res.status(200).send(user)
+  } catch (e) {
+    res.status(500).end()
+    pino.error(e)
+  }
 }
