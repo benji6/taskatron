0c2155594b945873deb4c30392614565366b7244
Ben Hall <benji2357@gmail.com>
2018-09-16 15:19:22 +0100

    chore(logging): remove morgan - will log with nginx or API Gateway instead of within node app

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,6 @@ import * as bodyParser from 'body-parser'
 import * as config from 'config'
 import * as cors from 'cors'
 import * as express from 'express'
-import * as morgan from 'morgan'
 import passwordless from './passwordless/index'
 import pino from './pino'
 import router from './router'
@@ -12,11 +11,6 @@ const port = config.get('port')
 
 const app = express()
 
-app.use(
-  morgan(
-    ':remote-addr :remote-user [:date] :method :url HTTP/:http-version :status :res[content-length] - :response-time ms',
-  ),
-)
 app.use(bodyParser.json())
 app.use(cors())
 app.use(passwordless.acceptToken())
