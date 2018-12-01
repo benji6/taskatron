ab8afa1c49fbbf8718f1ae5d81e6c7bef080056e
Ben Hall <benji2357@gmail.com>
2018-12-01 14:55:19 +0000

    chore(shared): add scripts and dependencies

--- shared/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports = radii = [3, 5, 10, 20, 30, 50]
-module.exports = maxServiceNameLength = 100
-module.exports = maxServiceDescriptionLength = 500
--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 set -e
 
+cd api && yarn test && echo api tests pass &
 cd client && yarn test && echo client tests pass &
-cd api && yarn test && echo api tests pass
+cd shared && yarn test && echo shared tests pass
 wait
