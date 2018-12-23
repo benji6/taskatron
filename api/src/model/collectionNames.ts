82cc72878799f987848de4e73b3e6c36aa1fc050
Ben Hall <benji2357@gmail.com>
2018-12-23 22:50:19 +0000

    refactor(api): rename passwordless-tokens collection to passwordless_tokens

--- api/src/model/collectionNames.ts
+++ api/src/model/collectionNames.ts
@@ -1,5 +1,5 @@
-export type TCollectionNames = 'passwordless-tokens' | 'services' | 'users'
+export type TCollectionNames = 'passwordless_tokens' | 'services' | 'users'
 
-export const PASSWORDLESS_TOKENS = 'passwordless-tokens'
+export const PASSWORDLESS_TOKENS = 'passwordless_tokens'
 export const SERVICES = 'services'
 export const USERS = 'users'
